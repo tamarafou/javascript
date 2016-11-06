8dcf00e885795c263efa23ddfd4172b03590589b
tamarafou <tamarafou@Tamaras-MacBook-Air.local>
2016-11-06 12:02:00 -0500

    javascript

---
+++ style.js
@@ -0,0 +1,14 @@
+var x = 0;
+
+function setup() {
+    createCanvas(400, 500);
+
+}
+function draw {(200,0,200);
+fill(0,250,0);
+stroke(255,255,255);
+strokeWeight(20);
+ellipse(x, height/2, 30,30);
+x++;
+               
+}
